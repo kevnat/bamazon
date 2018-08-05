6119df89c7defde29ebde80d8f3d114c4ff57166
Kevin Nathan <kevmaxnathan@gmail.com>
2018-08-04 22:40:48 -0400

    adding multiplier function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -38,60 +38,70 @@ function selectAll() {
 
 function promptUser(){
   inquirer
-    .prompt(//[
+    .prompt([
       {
         name:"selection",
         type:"input",
         message:"Please enter ID of the product you'd like to buy"
-      }//,
-      // {
-      //   name: "quantity",
-      //   type: "input",
-      //   message:"How many would you like to buy?"
-      // }
-   // ]
-  )
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message:"How many would you like to buy?"
+      }
+   ])
     .then(function(answer) {
-      // let purchase = {
-      //   item: answer.selection,
-      //   quantity: answer.quantity
-      // };
-      let input = answer.selection;
-      let quantity = answer.quantity; 
-      console.log(input); 
-      getProduct(input);
+      let purchase = {
+        item: answer.selection,
+        quantity: answer.quantity
+      };
+      // let input = answer.selection;
+      // let quantity = answer.quantity; 
+      console.log("\n-------------------------"+ "\nYou've selected" + purchase.quantity + " unit(s) of item_id: " + purchase.item); 
+      getProduct(purchase);
     });
   }
 
-function getProduct(input){
-    connection.query('SELECT product_name, stock_quantity FROM products WHERE item_id = ?',[input], function(error, result) {
+function getProduct(purchase){
+    connection.query('SELECT product_name, department_name, stock_quantity FROM products WHERE item_id = ?',[purchase.item], 
+    function(error, result) {
       if (error) throw error;
-      console.log("You've Selected: " + result[0].product_name + 
+      console.log("Product Name: " + result[0].product_name + " | " + result[0].department_name + "\n-------------------------"+ 
       "\nThere are "+ result[0].stock_quantity + " left in stock.");
       let currentStock = result[0].stock_quantity;
-      updateStock(currentStock,input);
+      updateStock(currentStock,purchase);
     });
   }
 
-  function updateStock(currentStock, input){
-    let newStock = currentStock - 1; 
-    console.log("Item removed from inventory");
-    console.log("There are now: " + newStock + " of item_id " + input);
-    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?',[newStock,input], function(error, result){
+  function updateStock(currentStock,purchase){
+    let newStock = currentStock - purchase.quantity; 
+    console.log(purchase.quantity + " Item(s) removed from inventory");
+    console.log("There are now: " + newStock + " of item_id " + purchase.item);
+    connection.query('UPDATE products SET stock_quantity = ? WHERE item_id = ?',[newStock,purchase.item], 
+    function(error, result){
       if (error) {
         throw error;
       } else if(newStock <= 0) {
         console.log("Insufficient Quantity!");
       } else {
-        console.log("Thank you for your purchase");
+        console.log("Thank you for your purchase" + 
+      "\n-------------------------");
       }
     })
-    connection.end();
-
+    displayCost(purchase);
   }
-  // function displayCost(){
-  //   connection.query("SELECT price")
-   
-    
-
-  // }
+  function displayCost(purchase){
+    connection.query('SELECT price FROM products WHERE item_id = ?',[purchase.item],
+    function(error,result){
+      if (error){
+        throw error;
+      } else {
+        console.log(
+          "# of Units: " + purchase.quantity + 
+          "\nUnit Cost: $" + result[0].price +
+          "\n-------------------------" + 
+          "\nTotal: $" + result[0].price * purchase.quantity);
+      }
+  })
+    connection.end();
+  }
